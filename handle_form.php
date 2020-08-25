f73655ed2d7ee5a08775b2a8c2c64e1aee755939
CristinaRiveraV <53008966+CristinaRiveraV@users.github.com>
2020-08-25 16:23:28 +0100

    php validating inputs

--- handle_form.php
+++ handle_form.php
@@ -1,49 +1,42 @@
-<html>
+<?php
+    $validation_error = "";
+    $success_message = "";
+  
+    $name = $email = $message = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      if (empty($_POST["name"])) {
+        $validation_error = "*Name is required";
+      } else {
+        $name = $_POST["formname"];
+      }
     
-<link rel="stylesheet" href="myfirststyle.css">
-<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Russo+One&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-<script src="myscript.js"></script>
+      validate_email();
+      validate_message();
+    }
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="description" content="portfolio - my first webpage">
-  <meta name="keywords" content="HTML, CSS, JavaScript">
-  <meta name="author" content="Cristina Rivera">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-  <title>Cristina's portfolio - Personal Profile</title>
-</head>
+  function validate_email(){
+    if (empty($_POST["femail"])) {
+      $validation_error = "Email is required";
+    } else {
+      if(filter_var($e, FILTER_VALIDATE_EMAIL)){
+        $email = test_input($_POST["femail"]);
+      }else{
 
-<body>
-  <header>
-    Cristina Rivera
-  </header>
+      }
+    } 
+  }
+  function validate_message(){
+    if (empty($_POST["fmessage"])) {
+      $validation_error = "*Please write a message";
+    } else {
+      if(strlen($message) >1000)){
+        $validation_error = "*The message can't be longer than a thouand characters.";
+      }else{
+        $message = $_POST["fmessage"];
+      }
+    }
 
-  <nav class="menu_bar">
-    <ul>
-      <li id="menu_item_home" class="menu_item"><a href="/index.html">Home</a></li>
-      <!--<li id="menu_item_projects" class="menu_item"><a href="htm_files/projects.html">Projects</a></li>-->
-      <li id="menu_item_cv" class="menu_item"><a href="cv.htm">My CV</a></li>
-      <li id="menu_item_contact" class="menu_item"><a href="contact.htm">Contact Me</a></li>
-    </ul>
-  </nav>
-
-  <main class="home_page">
-    <div class="form_response">
-      <h1 title="Hello!"><samp> Thank you! </samp></h1>
-      
-      <h2 title="Appologies"> The data is NOT being processed. Please send me an email. </h2>
-      
-      </div>
-  </main>
-
-
-  <footer >
-    <ul>
-      <li class="contact" id="email"><p>Email me at: cristina.rivera.valdez@gmail.com </p></li>
-      <!--<li class="contact" id="mobile"><p>Mobile: 07759405300</p></li>-->
-    </ul>
-  </footer>
-</body>
-</html>
+  }
+?>
